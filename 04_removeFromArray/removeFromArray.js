51ce832ac13c6e09f8da60ffb100a7be4826f5b5
Mark Vecchione <187446994+mark-vecchione@users.noreply.github.com>
2024-12-27 13:41:22 -0500

    Exercise 4 solution js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+    return arr.filter(item => !valuesToRemove.some(val => val === item));
 };
 
 // Do not edit below this line

5c166faa1ca645d06b9d02ddf23a68c6535c33ca
Mark Vecchione <187446994+mark-vecchione@users.noreply.github.com>
2024-12-30 11:26:10 -0500

    Exercise 8 solution js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,33 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args.reduce((add,current) => add + current, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args.reduce((difference,current) => difference - current);
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	let sums = 0;
+  for (let i = 0; i < inputArray.length; i++) {
+    sums += inputArray[i]; 
+  }
+  return sums
 };
 
-const multiply = function() {
+const multiply = function(...args) {
+  return args.reduce((product, current) => product * Number(current, 1);
+ };
 
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(fact) {
+  let result = 1;
+  for (let i = 1; i <= fact; i++) {
+    result = result * i;
+  }
+  return result;
 	
 };
 

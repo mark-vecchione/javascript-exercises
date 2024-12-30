4403abb12288ce2534cb79b9e03efd2e1cb18982
Mark Vecchione <187446994+mark-vecchione@users.noreply.github.com>
2024-12-30 12:18:25 -0500

    Exercise 10 solution js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    let memberNumber = Number(member);
+    if (memberNumber < 0) return "OOPS";
+    
+    const fibList = [0,1];
 
-};
+    for (let i = 2; i <= member; i++) {
+        fibList.push(fibList[i-1] + fibList[i-2]);
+    }
+    return fibList[memberNumber];
+}; 
 
 // Do not edit below this line
 module.exports = fibonacci;

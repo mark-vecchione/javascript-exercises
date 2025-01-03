b68798144f856ec6c43277104a47c0c338d22a65
Mark Vecchione <187446994+mark-vecchione@users.noreply.github.com>
2025-01-03 16:22:14 -0500

    Exercise 12 solution js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const calculateAges = function(people) {
+    const currentYear = new Date().getFullYear();
+    return people.map(person => {
+        const age = person.yearOfDeath
+            ? person.yearOfDeath - person.yearOfBirth
+            : currentYear - person.yearOfBirth;
+        return {
+            name: person.name,
+            age: age
+        };
+    });
+};
 
+
+const findTheOldest = function(people) {
+    const poeplesAges = calculateAges(people);
+    return poeplesAges.reduce((oldest, person) => {
+        return person.age > oldest.age ? person: oldest;
+    });
 };
 
 // Do not edit below this line
